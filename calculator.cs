d08b9b2d6815bf3706d98b4af86a14e343425522
bintangalfaruqi <bintang.al@students.amikom.ac.id>
2020-03-10 10:17:47 +0700

    perubahan pegurangann

--- calculator.cs
+++ calculator.cs
@@ -2,10 +2,16 @@
 {
  static vold main (string[]args)
 	}
-
+	[
 	static int penambahan (int a,int b)
 	{
 		return a + b;
+	
+	}
+
+	static int penambahan (int a,int b)
+	{
+		return a - b;
 	
 	}
 }
